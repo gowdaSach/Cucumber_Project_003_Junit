fa30b7bf18e60e2131a91afa84bedf3827252e38
gowdaSach <sachingowda270496@gmail.com>
2022-10-06 14:53:15 +0530

    First Commit

---
+++ test-output-thread/report.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "15c0683f-1768-4771-b690-70436eb043c1",
+    "feature": "Login page Feature",
+    "scenario": "Login Page Title",
+    "start": 1665044407906,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1665044417929,
+    "className": "failed"
+  },
+  {
+    "id": "0e74c2de-8170-4f6d-9c39-b2cc995d31b7",
+    "feature": "Login page Feature",
+    "scenario": "Login with valid Credentials",
+    "start": 1665044423564,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1665044429078,
+    "className": "failed"
+  },
+  {
+    "id": "edaee527-7531-4a7b-ae4e-0ae751fd69bc",
+    "feature": "Login page Feature",
+    "scenario": "Forgot Password Link",
+    "start": 1665044417959,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1665044423547,
+    "className": "failed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 1,
+    "content": "Thread[main,5,main]"
+  }
+]);
+});
